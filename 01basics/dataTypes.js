19fd4bd05cce90af252db4798296c455b38698b3
AnkitRay-Dev <164015889+ankitray-dev@users.noreply.github.com>
2024-05-07 09:58:56 +0530

    learned datatypes

---
+++ 01basics/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // This line treat all JS code as newer version
+
+// alert ( 3 + 3 ) // It will work on browser not on nodejs
+
+console.log(3
+    
+    +3) // Always write the code in a readable manner
+
+console.log("Ankit")
+
+let name = "ankit"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// Primitive Datatype
+
+// Number => 2 to power 53
+// Bigint
+// String => ""
+// Boolean => True/False
+// Null => standalone value
+// Undefined =>
+// Symbol => unique
+
+console.log(typeof "ankit")
+console.log(typeof undefined) // Type = Undefined
+console.log(typeof null) // Type = Object
+
+// Object
